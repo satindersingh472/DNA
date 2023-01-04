acb95a0cd3099d5220128013c29fd3d22ff838cc
satinder <satindersingh472@gmail.com>
2023-01-04 09:20:51 -0700

    finished_protein_project

--- src/DNA.java
+++ src/DNA.java
@@ -12,11 +12,20 @@ public class DNA {
         String dna1 = "ATGCGATACGCTTGA";
         String dna2 = "ATGCGATACGTGA";
         String dna3 =  "ATTAATATGTACTGA";
-        String dna = dna1;
+        String dna = dna3;
         int length = dna.length();
         System.out.println("Length: " + length);
         int start  = dna.indexOf("ATG");
-        System.out.println(start);
+//        System.out.println(start);
+        int stop = dna.indexOf("TGA");
+//        System.out.println(stop);
+
+        if(start != -1 && stop != -1 && (stop - start) % 3 == 0){
+            String protein = dna.substring(start,stop + 3);
+            System.out.println("Protein: " + protein );
+        } else {
+            System.out.println("Not a protein");
+        }
 
     }
 
