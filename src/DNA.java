06f3deb1fefbe6278194e7f0e13c27f4410fb19e
satinder <satindersingh472@gmail.com>
2023-01-04 09:23:20 -0700

    commenting_done

--- src/DNA.java
+++ src/DNA.java
@@ -13,13 +13,16 @@ public class DNA {
         String dna2 = "ATGCGATACGTGA";
         String dna3 =  "ATTAATATGTACTGA";
         String dna = dna3;
+//        will check the length just for testing
         int length = dna.length();
         System.out.println("Length: " + length);
+//        will check the start index of atg codon
         int start  = dna.indexOf("ATG");
-//        System.out.println(start);
+//        will check the tga codon index
         int stop = dna.indexOf("TGA");
-//        System.out.println(stop);
 
+//      check if atg and tga are the substring or not and codon between start and stop is divisible by 3
+//        if yes then it contains a protein
         if(start != -1 && stop != -1 && (stop - start) % 3 == 0){
             String protein = dna.substring(start,stop + 3);
             System.out.println("Protein: " + protein );
