69694b6bb72dcb5c1f47c0c243d6daac822918a3
satinder <satindersingh472@gmail.com>
2023-01-04 08:37:40 -0700

    working_on_index

--- src/DNA.java
+++ src/DNA.java
@@ -1,2 +1,23 @@
+
+//this program will determine weather there is a protein in a strand of dna
 public class DNA {
+
+    public static void main(String[] args) {
+
+        //  -. .-.   .-. .-.   .
+        //    \   \ /   \   \ /
+        //   / \   \   / \   \
+        //  ~   `-~ `-`   `-~ `-
+
+        String dna1 = "ATGCGATACGCTTGA";
+        String dna2 = "ATGCGATACGTGA";
+        String dna3 =  "ATTAATATGTACTGA";
+        String dna = dna1;
+        int length = dna.length();
+        System.out.println("Length: " + length);
+        int start  = dna.indexOf("ATG");
+        System.out.println(start);
+
+    }
+
 }
